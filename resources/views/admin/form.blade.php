a3ba51d029ef88d60ac1d65294d9dddf1003c7e0
Jeroen Noten <jeroennoten@me.com>
2016-08-20 20:47:16 +0200

    ckeditor config

--- resources/views/admin/form.blade.php
+++ resources/views/admin/form.blade.php
@@ -26,6 +26,48 @@
 </div>
 
 @section('js')
-    @ckeditor('summaryField', ['height' => 150])
-    @ckeditor('bodyField', ['height' => 300])
+    @ckeditor('summaryField', [
+        'height' => 150,
+        'toolbarGroups' => [
+            [ 'name' => 'document', 'groups' => [ 'mode', 'document', 'doctools' ] ],
+            [ 'name' => 'document', 'groups' => [ 'mode', 'document', 'doctools' ] ],
+            [ 'name' => 'clipboard', 'groups' => [ 'clipboard', 'undo' ] ],
+            [ 'name' => 'editing', 'groups' => [ 'find', 'selection', 'spellchecker', 'editing' ] ],
+            [ 'name' => 'forms', 'groups' => [ 'forms' ] ],
+            '/',
+            [ 'name' => 'basicstyles', 'groups' => [ 'basicstyles', 'cleanup' ] ],
+            [ 'name' => 'paragraph', 'groups' => [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] ],
+            [ 'name' => 'links', 'groups' => [ 'links' ] ],
+            [ 'name' => 'insert', 'groups' => [ 'insert' ] ],
+            '/',
+            [ 'name' => 'styles', 'groups' => [ 'styles' ] ],
+            [ 'name' => 'colors', 'groups' => [ 'colors' ] ],
+            [ 'name' => 'tools', 'groups' => [ 'tools' ] ],
+            [ 'name' => 'others', 'groups' => [ 'others' ] ],
+            [ 'name' => 'about', 'groups' => [ 'about' ] ]
+        ],
+        'removeButtons' => 'Save,NewPage,Preview,Print,Templates,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Blockquote,CreateDiv,BidiLtr,BidiRtl,Language,Flash,Smiley,PageBreak,Iframe'
+    ])
+    @ckeditor('bodyField', [
+        'height' => 300,
+        'toolbarGroups' => [
+            [ 'name' => 'document', 'groups' => [ 'mode', 'document', 'doctools' ] ],
+            [ 'name' => 'document', 'groups' => [ 'mode', 'document', 'doctools' ] ],
+            [ 'name' => 'clipboard', 'groups' => [ 'clipboard', 'undo' ] ],
+            [ 'name' => 'editing', 'groups' => [ 'find', 'selection', 'spellchecker', 'editing' ] ],
+            [ 'name' => 'forms', 'groups' => [ 'forms' ] ],
+            '/',
+            [ 'name' => 'basicstyles', 'groups' => [ 'basicstyles', 'cleanup' ] ],
+            [ 'name' => 'paragraph', 'groups' => [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] ],
+            [ 'name' => 'links', 'groups' => [ 'links' ] ],
+            [ 'name' => 'insert', 'groups' => [ 'insert' ] ],
+            '/',
+            [ 'name' => 'styles', 'groups' => [ 'styles' ] ],
+            [ 'name' => 'colors', 'groups' => [ 'colors' ] ],
+            [ 'name' => 'tools', 'groups' => [ 'tools' ] ],
+            [ 'name' => 'others', 'groups' => [ 'others' ] ],
+            [ 'name' => 'about', 'groups' => [ 'about' ] ]
+        ],
+        'removeButtons' => 'Save,NewPage,Preview,Print,Templates,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Blockquote,CreateDiv,BidiLtr,BidiRtl,Language,Flash,Smiley,PageBreak,Iframe'
+    ])
 @append
